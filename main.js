ab1c6c63bdff782c896b9a12d7dee9a2655a82b9
Sakhil Omarov <sakhil.omarov.1@iliauni.edu.ge>
2026-03-01 13:19:59 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let temperature = 0;
+
+if (temperature < 0){
+
+    console.log("ყინავს!");
+} else if(temperature >=0 && temperature <= 10){
+    console.log("ძალიან ცივა")
+} else if(temperature >=11 && temperature <= 20){
+    console.log("ცივა");
+} else if(temperature >= 21 && temperature <= 30){
+    console.log("თბილ");
+} else {
+    console.log("ცხელა!");
+} 
